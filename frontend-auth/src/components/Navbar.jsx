223078e7c392ca42a6604247822dbb4c2228bd05
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-01-16 16:02:29 +0530

    added login page

--- frontend-auth/src/components/Navbar.jsx
+++ frontend-auth/src/components/Navbar.jsx
@@ -1,20 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../../public/logo.avif"
 
 const Navbar = () => {
-
   const navigate = useNavigate();
 
   return (
     <div className="w-full bg-gray-50 shadow-blue-100 shadow-md h-20 flex justify-between items-center px-4 py-2 sm:p-6 sm:px-24 absolute top-0">
-      <div className="flex items-center relative"><img src={logo} className="w-20 sm:w-20" alt="logo" /><span className="font-bold text-2xl text-blue-800 absolute left-4 top-7"><i>Auth</i></span></div>
-      <button  onClick={() => navigate('/login')}
-      className="flex font-semibold text-blue-700 items-center gap-2 border border-gray-500 bg-gray-200 rounded-full px-6
-      py-2  hover:bg-gray-300 transition-all">Login <span class="material-symbols-outlined transition-all">trending_flat</span></button>
-    </div>
-  )
+      
+      {/* Logo Section */}
+      <div className="flex items-center relative">
+        <span className="material-symbols-outlined  logo-size-css text-gray-900">
+          assured_workload
+        </span>
+        <span className="font-bold text-2xl text-blue-400 absolute left-12 top-2">
+          <i>Auth</i>
+        </span>
+      </div>
 
-  
-}
+      {/* Login Button */}
+      <button
+        onClick={() => navigate("/login")}
+        className="flex font-semibold text-blue-700 items-center gap-2 border border-gray-500 bg-gray-200 rounded-full px-6 py-2 hover:bg-gray-300 transition-all"
+      >
+        Login
+        <span className="material-symbols-outlined text-xs text-gray-700 transition-all">
+          trending_flat
+        </span>
+      </button>
+    </div>
+  );
+};
 
 export default Navbar;
--- frontend-auth/src/pages/Login.jsx
+++ frontend-auth/src/pages/Login.jsx
@@ -1,8 +1,76 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const [state, setState] = useState('Sign Up');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Login</div>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-gray-400 to-pink-500">
+      <p className="absolute left-5 sm:left-20 top-5 cursor-pointer">
+        <span onClick={() => navigate('/')}
+          className="material-symbols-outlined logo-size-css text-gray-900">
+          assured_workload
+        </span>
+      </p>
+
+     <div className="bg-slate-800 p-10 rounded-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-2xl font-semibold text-white text-center mb-2">{state === 'Sign Up' ? "Create Account" : "Login Account!"} </h2>
+        <p className="text-center mb-1.5">{state === 'Sign Up' ? "Create your account" : "Login to your account!"}</p>
+
+        <form >
+
+          {
+            state === 'Sign Up' && (
+              <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+                <span class="material-symbols-outlined text-amber-50">person</span>
+                <input  onChange={e => setName(e.target.value)} value={name}
+                  className="bg-transparent outline-none" type="text" placeholder="Full Name" required />
+              </div>
+            )
+          }
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <span class="material-symbols-outlined text-amber-50">mark_email_unread</span>
+            <input  onChange={e => setEmail(e.target.value)} value={email}
+            className="bg-transparent outline-none" type="email" placeholder="Email id" required />
+          </div>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <span class="material-symbols-outlined text-amber-50">lock</span>
+            <input onChange={e => setPassword(e.target.value)} value={password}
+            className="bg-transparent outline-none" type="password" placeholder="Password" required />
+          </div>
+
+          <p onClick={() => navigate('/reset-password')} className="mb-4 text-indigo-300 cursor-pointer">Forgot Password?</p>
+
+          <button className="w-full py-2 cursor-pointer rounded-full bg-gradient-to-r text-amber-50 font-medium from-blue-600 to-purple-800">
+            {state}
+          </button>
+
+        </form>
+
+        {
+          state === 'Sign Up' ? (
+            <p className="text-center text-gray-400 text-xs  mt-2.5">Already have an account? {' '} 
+              <span onClick={() => setState('Login')} className="text-blue-400 cursor-pointer  underline">Login here</span>
+            </p>
+
+          ) : (
+            <p className="text-center text-gray-400 text-xs  mt-2.5">Don't have an account? {' '} 
+              <span onClick={() => setState('Sign Up')} className="text-blue-400 cursor-pointer  underline">Sign Up</span>
+            </p>
+          )
+        }
+
+     </div>
+    </div>
   )
 }
+
 
 export default Login
